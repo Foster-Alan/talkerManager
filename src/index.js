85b27a0baaa9a9af7560c90530b00c5471faea61
Alan Foster <alan-af@outlook.com>
2022-12-14 15:22:25 -0300

    1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,12 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionarr
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+// GET 
+app.get('/talker', async (req, res) => {
+  const data = JSON.parse(fs.readFileSync('src/talker.json', 'utf8'));
+  res.status(200).json(data);
 });
 
 app.listen(PORT, () => {
